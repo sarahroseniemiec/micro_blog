e109e987057c67655e6dca42700133856c229e98
sarah niemiec <sroseniemiec@gmail.com>
2017-04-19 19:03:39 -0400

    sign in working

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require "bundler/setup"
 require "sinatra/flash"
 
 set :database, "sqlite3:microblog.sqlite3"
+set :sessions, true
 
 get "/" do
   erb :homepage
@@ -26,8 +27,29 @@ get "/sign_up" do
   erb :sign_up
 end
 
+get "/signin" do
+  erb :sign_in
+end
+
 post "/sign-up" do
   params.inspect
   User.create(username: params[:username], password: params[:password])
   redirect "/"
+end
+
+post "/create-post" do
+  params.inspect
+  Post.create(content: params[:content])
+  redirect "/post"
+end
+
+post "/sign-in" do
+  params.inspect
+  @user = User.where(username: params[:username]).first
+  if @user && @user.password == params[:password]
+    # session[:user_id] = @user.id
+    redirect "/"
+  else
+    redirect "/post"
+  end
 end
