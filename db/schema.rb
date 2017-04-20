822078c5a5f5297bfd1406e6504f2926a1c7fd7b
sarah niemiec <sroseniemiec@gmail.com>
2017-04-19 20:12:59 -0400

    fixed posts tble

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419202029) do
+ActiveRecord::Schema.define(version: 20170420000451) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "date"
     t.string   "content"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
